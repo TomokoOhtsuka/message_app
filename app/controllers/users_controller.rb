3168c23dbbb787393ee6077ba95f2bf3e8795a97
大塚智子 <tomoko@ootsukatomokonoMacBook-puro.local>
2019-01-30 00:38:19 +0900

    initialize commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   end
 
   def create
+   - binding.pry
    @user = User.new(user_params)
    return redirect_to user_path(@user) if @user.save
    render action: 'new'
